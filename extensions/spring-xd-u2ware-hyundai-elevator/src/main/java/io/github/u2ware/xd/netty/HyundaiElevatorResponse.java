cea0396c75301a8143f5af7e7526a0586b1e6205
U2ware <u2waremanager@gmail.com>
2015-10-22 22:02:35 +0900

    u2ware-hyundai-elevator

--- extensions/spring-xd-u2ware-hyundai-elevator/src/main/java/io/github/u2ware/xd/netty/HyundaiElevatorResponse.java
+++ extensions/spring-xd-u2ware-hyundai-elevator/src/main/java/io/github/u2ware/xd/netty/HyundaiElevatorResponse.java
@@ -1,8 +1,8 @@
 package io.github.u2ware.xd.netty;
 
-import io.github.u2ware.integration.common.BuildingAutomationSystemData;
+import java.io.Serializable;
 
-public class HyundaiElevatorResponse implements BuildingAutomationSystemData{
+public class HyundaiElevatorResponse implements Serializable{
 
 	private static final long serialVersionUID = 1325956282725736704L;
 
