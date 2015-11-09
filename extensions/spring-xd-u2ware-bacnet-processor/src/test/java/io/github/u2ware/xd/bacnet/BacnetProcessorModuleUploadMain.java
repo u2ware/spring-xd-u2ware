484971e3677adef346ac5705c063076438e25956
U2ware <u2waremanager@gmail.com>
2015-11-09 17:17:22 +0900

    u2ware-bacnet-processor

--- extensions/spring-xd-u2ware-bacnet-processor/src/test/java/io/github/u2ware/xd/bacnet/BacnetProcessorModuleUploadMain.java
+++ extensions/spring-xd-u2ware-bacnet-processor/src/test/java/io/github/u2ware/xd/bacnet/BacnetProcessorModuleUploadMain.java
@@ -55,6 +55,9 @@ public class BacnetProcessorModuleUploadMain {
 		logger.info("ModuleUploadPlugin module uploaded!!! ");
 		
 		application.close();
+
+		Thread.sleep(3000);
+		System.exit(0);
     }
 }
 
