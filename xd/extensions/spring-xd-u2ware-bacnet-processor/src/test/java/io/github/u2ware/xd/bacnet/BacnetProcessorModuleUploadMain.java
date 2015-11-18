d5e736af6c8ad9e18e2dae982d48d214fce8555f
U2ware <u2waremanager@gmail.com>
2015-11-18 16:23:45 +0900

    u2ware-bacnet-processor

--- xd/extensions/spring-xd-u2ware-bacnet-processor/src/test/java/io/github/u2ware/xd/bacnet/BacnetProcessorModuleUploadMain.java
+++ xd/extensions/spring-xd-u2ware-bacnet-processor/src/test/java/io/github/u2ware/xd/bacnet/BacnetProcessorModuleUploadMain.java
@@ -20,8 +20,8 @@ public class BacnetProcessorModuleUploadMain {
     private static final String moduleType     = "processor";
     private static final String moduleResource = "target/spring-xd-u2ware-bacnet-processor-1.0.0.jar";
 
-    private static final String sourceResource     = "../custom-modules/";
-    private static final String targetResource     = "../custom-modules/";
+    private static final String sourceResource     = "../../custom-modules/";
+    private static final String targetResource     = "../../custom-modules/";
     
     public static void main(String[] args) throws Exception{
     	logger.info("ModuleUploadPlugin starting.....");
