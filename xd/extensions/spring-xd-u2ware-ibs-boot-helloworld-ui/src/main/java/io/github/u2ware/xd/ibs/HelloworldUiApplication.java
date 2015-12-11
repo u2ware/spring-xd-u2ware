7a46088ca9e425422b2bef93447ced041cd487c4
U2ware <u2waremanager@gmail.com>
2015-12-11 14:14:56 +0900

    custom-modules

---
+++ xd/extensions/spring-xd-u2ware-ibs-boot-helloworld-ui/src/main/java/io/github/u2ware/xd/ibs/HelloworldUiApplication.java
@@ -0,0 +1,13 @@
+package io.github.u2ware.xd.ibs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldUiApplication {
+	
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldUiApplication.class, args);
+        //JettyEmbeddedServletContainer f;
+    }
+}
