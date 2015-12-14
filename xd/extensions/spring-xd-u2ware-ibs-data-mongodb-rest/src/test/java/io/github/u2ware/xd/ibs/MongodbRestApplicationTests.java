d1a07dc41c2fd8825e9d8b5008d06aa73dbf8e2f
U2ware <u2waremanager@gmail.com>
2015-12-14 17:47:46 +0900

    custom-modules

---
+++ xd/extensions/spring-xd-u2ware-ibs-data-mongodb-rest/src/test/java/io/github/u2ware/xd/ibs/MongodbRestApplicationTests.java
@@ -0,0 +1,57 @@
+package io.github.u2ware.xd.ibs;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MongodbRestApplication.class)
+@WebAppConfiguration
+public class MongodbRestApplicationTests {
+
+    @BeforeClass
+	public static void beforeClass() throws Exception {
+		MongodbServer.startup(27017);
+	}
+	
+	@AfterClass
+	public static void afterClass() throws Exception {
+		MongodbServer.shutdown();
+	}
+
+	@Autowired
+	private WebApplicationContext context;
+
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() {
+		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+	}
+
+    @Test
+	public void contextLoads() throws Exception {
+
+		this.mvc.perform(
+						get("/helloworld")
+				).andDo(
+						print()
+				).andExpect(
+						status().isOk()
+				);
+    }    
+
+}
---
+++ xd/extensions/spring-xd-u2ware-ibs-data-mongodb-rest/src/test/java/io/github/u2ware/xd/ibs/MongodbRestModuleIntegrationTest.java
@@ -0,0 +1,85 @@
+package io.github.u2ware.xd.ibs;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.xd.dirt.server.singlenode.SingleNodeApplication;
+import org.springframework.xd.dirt.test.SingleNodeIntegrationTestSupport;
+import org.springframework.xd.dirt.test.SingletonModuleRegistry;
+import org.springframework.xd.dirt.test.process.SingleNodeProcessingChainConsumer;
+import org.springframework.xd.dirt.test.process.SingleNodeProcessingChainSupport;
+import org.springframework.xd.module.ModuleType;
+
+public class MongodbRestModuleIntegrationTest {
+	
+    protected Log logger = LogFactory.getLog(getClass());
+
+    private static SingleNodeApplication application;
+
+
+	/**
+	 * Start the single node container, binding random unused ports, etc. to not conflict with any other instances
+	 * running on this host. Configure the ModuleRegistry to include the project module.
+	 */
+	@BeforeClass
+	public static void beforeClass() throws Exception{
+		MongodbServer.startup(27017);
+		//RandomConfigurationSupport randomConfigSupport = new RandomConfigurationSupport();
+		application = new SingleNodeApplication().run();
+		
+		SingleNodeIntegrationTestSupport singleNodeIntegrationTestSupport 
+			= new SingleNodeIntegrationTestSupport(application);
+		singleNodeIntegrationTestSupport.addModuleRegistry(
+				new SingletonModuleRegistry(ModuleType.source, "ibs-data-mongodb-rest"));
+	}
+
+	
+	@AfterClass
+	public static void afterClass() throws Exception{
+		MongodbServer.shutdown();
+	}
+	
+
+	@Test
+	public void test() throws InterruptedException {
+
+		String streamName = "streamTest";
+
+		String processingChainUnderTest = "ibs-data-mongodb-rest "
+				+ " --httpPort=9899 ";
+
+		logger.debug(processingChainUnderTest);
+		
+		SingleNodeProcessingChainConsumer chain = SingleNodeProcessingChainSupport.chainConsumer(application, streamName, processingChainUnderTest);
+		//SingleNodeProcessingChain chain = SingleNodeProcessingChainSupport.chain(application, streamName, processingChainUnderTest);
+		//SingleNodeProcessingChainProducer chain = SingleNodeProcessingChainSupport.chainProducer(application, streamName, processingChainUnderTest);
+		
+		
+		Thread.sleep(3000);
+		try{
+			RestTemplate restTemplate = new RestTemplate();
+			String result = restTemplate.getForObject("http://localhost:9899/helloworld", String.class);
+			logger.debug(result);
+			Assert.assertEquals("hello world", result);
+		}catch(Exception e){
+			
+		}
+		
+//		List<?> result4 = restTemplate.postForObject("http://localhost:9998/personDb/person","{'name':'Joe'}", List.class);
+//		logger.debug(result4);
+//		Assert.assertEquals(1, result4.size());
+
+		
+
+		
+
+		chain.destroy();
+	}
+}
+
+
+
