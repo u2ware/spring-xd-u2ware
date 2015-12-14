1075fd827c3075514ee81ed341070b2f8f1859e5
U2ware <u2waremanager@gmail.com>
2015-12-14 13:18:14 +0900

    custom-modules

--- xd/extensions/spring-xd-u2ware-ibs-boot-sample/src/main/java/io/github/u2ware/xd/ibs/person/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.u2ware.xd.ibs.person;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
-	
-}
